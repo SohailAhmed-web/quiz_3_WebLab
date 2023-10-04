8cb2ac04d90d8919ea691480bf51b446f1495c17
SohailAhmed-web <sohail.bscsf19@iba-suk.edu.pk>
2023-10-04 15:00:11 +0500

    images added to img folder

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,4 @@
 
-// Get the sign up form
 const signupForm = document.querySelector('form');
 
 // Add a submit event listener to the form
@@ -24,3 +23,18 @@ signupForm.addEventListener('submit', function(event) {
   // Alert the user that the form has been submitted successfully
   alert('Form submitted successfully!');
 });
+
+// Add a click event listener to the social media buttons
+const socialMediaButtons = document.querySelectorAll('.social-media-button');
+
+for (const button of socialMediaButtons) {
+  button.addEventListener('click', function(event) {
+    // Prevent the default link behavior
+    event.preventDefault();
+
+    // TODO: Implement social media login
+
+    // Alert the user that they have clicked on a social media button
+    alert('You have clicked on the ' + button.classList[1] + ' button.');
+  });
+}
